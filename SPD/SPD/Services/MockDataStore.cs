9c589b69d947a11d4325a3c4fdbf2c0a2c3d4c1f
Edward Dwayne Rush <edward.rush.developer@live.com>
2018-11-14 21:03:57 -0500

    Made Changes to the Mock Data Store.

--- SPD/SPD/Services/MockDataStore.cs
+++ SPD/SPD/Services/MockDataStore.cs
@@ -15,7 +15,7 @@ namespace SPD.Services
             items = new List<Item>();
             var mockItems = new List<Item>
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Main Menu item", Description="Main Menu Description." },
                 new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
                 new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
                 new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
